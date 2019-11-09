5b3e23199d3f761eb58233aafce3fc0eaaa2cf86
KimHoseong <ghtjd000129@naver.com>
2019-11-09 15:43:08 +0900

    몬스터 데이터베이스 -> 오브젝트 자동화

---
+++ Assets/Hoseong/Scripts/Common/Define.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDatabase<T> where T : IData
+{
+    void ClearDatabase();
+    void AddData(T data);
+    T GetData(int idx);
+}
+
+public interface IData
+{
+    void SetData(Dictionary<string, object> parsedData);
+}
---
+++ Assets/Hoseong/Scripts/Data/MonsterDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterDatabase : ScriptableObject, IDatabase<MonsterData>
+{
+    public List<MonsterData> monsters = new List<MonsterData>();
+
+    public void ClearDatabase()
+    {
+        monsters.Clear();
+    }
+
+    public void AddData(MonsterData data)
+    {
+        monsters.Add(data);
+    }
+
+    public MonsterData GetData(int id)
+    {
+        return monsters[id];
+    }
+}
