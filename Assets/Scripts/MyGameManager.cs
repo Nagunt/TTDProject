d1849550060ea56aa892508230f4cde5fd17aeec
Nagunt <reiy0902@naver.com>
2019-11-09 17:15:30 +0900

    GameManager, Tower

---
+++ Assets/Scripts/MyGameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameManager : MonoBehaviour
+{
+    public int wave;
+    public float difficulty;
+    public MySpawner[] spawners;
+    public int time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wave = 1;
+        time = 300;
+        StartCoroutine(Routine());
+        StartCoroutine(TimeRoutine());
+    }
+
+    IEnumerator Routine()
+    {
+        int timeWave;
+        switch (wave)   // 웨이브 별 구현
+        {
+            case 1:
+                {
+                    timeWave = 200;
+                    while(time > 0 && timeWave > 0)
+                    {
+                        for(int i = 0; i < spawners.Length; ++i)
+                        {
+                            //스포너
+                            spawners[i].Spawn(1);
+                        }
+                        yield return new WaitForSeconds(1f);
+                        timeWave--;
+                    }
+                    
+                    break;
+                }
+        }
+    }
+    
+    IEnumerator TimeRoutine()
+    {
+        WaitForSeconds waitOneSecond = new WaitForSeconds(1f);
+        while(time > 0)
+        {
+            yield return waitOneSecond;
+            time--;
+        }
+    }
+}
---
+++ Assets/Scripts/MyTower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTower : MonoBehaviour
+{
+    protected float delay;
+
+    private void Start()
+    {
+        delay = 0;
+    }
+
+    private void Update()
+    {
+        if (delay > 0) delay -= Time.deltaTime;
+    }
+
+    public virtual void Attack()
+    {
+
+
+    }
+
+    public virtual void Remove()
+    {
+        StopAllCoroutines();
+        Destroy(gameObject);
+    }
+}
