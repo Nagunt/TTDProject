f259d3167da6d1d6e6738b9e5b805509f5b61185
Nagunt <reiy0902@naver.com>
2019-11-10 15:28:08 +0900

    메인 Scene 머충 완성.

---
+++ Assets/Scripts/MyGameStarter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyGameStarter : MonoBehaviour
+{
+    public void OnClick_Start()
+    {
+        SceneManager.LoadScene("Scene_Play");
+    }
+}
