d2910493613677c025e3be1e358ac71a1559b3b7
Nagunt <reiy0902@naver.com>
2019-11-10 15:12:29 +0900

    번개 타워 랜덤 타겟팅 수정.

--- Assets/Scripts/Tower/MyTower_Thunder.cs
+++ Assets/Scripts/Tower/MyTower_Thunder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MyTower_Thunder : MyTower
 {
     private float delay = 0;
+    private Monster target;
     [Header("- Attribute")]
     [SerializeField]
     private LineRenderer thunderEffect;
@@ -17,17 +18,12 @@ public class MyTower_Thunder : MyTower
     private IEnumerator _Attack()
     {
         delay = attackDelay;
-        List<Collider2D> collider2Ds = new List<Collider2D>();
-        ContactFilter2D contactFilter2D = new ContactFilter2D();
-        contactFilter2D.SetLayerMask(LayerMask.GetMask("Monster"));
-        if (Physics2D.OverlapCollider(attackArea, contactFilter2D, collider2Ds) > 0)
+        if(target != null)
         {
-            Monster target = collider2Ds[0].GetComponent<Monster>();
-            target?.GetDamage(attack);
-
             thunderEffect.enabled = true;
             thunderEffect.positionCount = 2;
             thunderEffect.SetPositions(new Vector3[] { transform.position, target.transform.position });
+            target?.GetDamage(attack);
             yield return new WaitForSeconds(0.1f);
             thunderEffect.positionCount = 0;
             thunderEffect.enabled = false;
@@ -53,6 +49,29 @@ public class MyTower_Thunder : MyTower
             contactFilter2D.SetLayerMask(LayerMask.GetMask("Monster"));
             if (Physics2D.OverlapCollider(attackArea, contactFilter2D, collider2Ds) > 0)
             {
+                if (target == null)
+                {
+                    target = collider2Ds[0].GetComponent<Monster>();
+                }
+                else
+                {
+                    bool isTarget = false;
+                    // 타겟이 범위 안에 계속 존재하면 유지한다.
+                    for (int i = 0; i < collider2Ds.Count; ++i)
+                    {
+                        Monster tempMonster = collider2Ds[i].GetComponent<Monster>();
+                        if (tempMonster == null) continue;
+                        if (tempMonster == target)
+                        {
+                            isTarget = true;
+                            break;
+                        }
+                    }
+                    if (isTarget == false)
+                    {
+                        target = collider2Ds[0].GetComponent<Monster>();
+                    }
+                }
                 if (delay <= 0)
                 {
                     Attack();
